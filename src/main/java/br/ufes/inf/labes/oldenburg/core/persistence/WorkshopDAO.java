9570b2dabe50b84014a70546f17f2e95681ee308
RafaBaker <rafael1309mt@gmail.com>
2026-04-14 22:45:07 -0300

    começando autenticação

--- src/main/java/br/ufes/inf/labes/oldenburg/core/persistence/WorkshopDAO.java
+++ src/main/java/br/ufes/inf/labes/oldenburg/core/persistence/WorkshopDAO.java
@@ -1,9 +1,13 @@
 package br.ufes.inf.labes.oldenburg.core.persistence;
 
 import br.ufes.inf.labes.jbutler.ejb.persistence.BaseDAO;
+import br.ufes.inf.labes.jbutler.ejb.persistence.PersistentObjectSupport;
+import br.ufes.inf.labes.jbutler.ejb.persistence.exceptions.MultiplePersistentObjectsFoundException;
+import br.ufes.inf.labes.jbutler.ejb.persistence.exceptions.PersistentObjectNotFoundException;
 import br.ufes.inf.labes.oldenburg.core.domain.Workshop;
 import jakarta.ejb.Local;
 
 @Local
 public interface WorkshopDAO extends BaseDAO<Workshop> {
+    Workshop retrieveByAcronym(String acronym) throws PersistentObjectNotFoundException, MultiplePersistentObjectsFoundException;
 }
--- src/main/java/br/ufes/inf/labes/oldenburg/core/persistence/WorkshopJPADAO.java
+++ src/main/java/br/ufes/inf/labes/oldenburg/core/persistence/WorkshopJPADAO.java
@@ -1,10 +1,16 @@
 package br.ufes.inf.labes.oldenburg.core.persistence;
 
 import br.ufes.inf.labes.jbutler.ejb.persistence.BaseJPADAO;
+import br.ufes.inf.labes.jbutler.ejb.persistence.exceptions.MultiplePersistentObjectsFoundException;
+import br.ufes.inf.labes.jbutler.ejb.persistence.exceptions.PersistentObjectNotFoundException;
 import br.ufes.inf.labes.oldenburg.core.domain.Workshop;
+import br.ufes.inf.labes.oldenburg.core.domain.Workshop_;
 import jakarta.ejb.Stateless;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
 
 @Stateless
 public class WorkshopJPADAO extends BaseJPADAO<Workshop> implements WorkshopDAO {
@@ -14,5 +20,16 @@ public class WorkshopJPADAO extends BaseJPADAO<Workshop> implements WorkshopDAO 
     @Override
     protected EntityManager getEntityManager() {
         return this.entityManager;
+    }
+
+    @Override
+    public Workshop retrieveByAcronym(String acronym) throws PersistentObjectNotFoundException, MultiplePersistentObjectsFoundException {
+        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Workshop> cq = cb.createQuery(Workshop.class);
+        Root<Workshop> root = cq.from(Workshop.class);
+
+        cq.where(cb.equal(root.get(Workshop_.acronym), acronym));
+        Workshop result = executeSingleResultQuery(cq, acronym);
+        return result;
     }
 }
