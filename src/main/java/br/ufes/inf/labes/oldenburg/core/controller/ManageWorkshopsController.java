77d8cc66ec8b503ad82f7db7fee10607117835c7
RafaBaker <rafael1309mt@gmail.com>
2026-04-14 22:18:34 -0300

    alguns testes com botão de adicionar aleatoriamente

--- src/main/java/br/ufes/inf/labes/oldenburg/core/controller/ManageWorkshopsController.java
+++ src/main/java/br/ufes/inf/labes/oldenburg/core/controller/ManageWorkshopsController.java
@@ -5,8 +5,12 @@ import br.ufes.inf.labes.jbutler.ejb.controller.CrudController;
 import br.ufes.inf.labes.oldenburg.core.application.ManageWorkshopsService;
 import br.ufes.inf.labes.oldenburg.core.domain.Workshop;
 import jakarta.ejb.EJB;
+import jakarta.faces.application.FacesMessage;
 import jakarta.inject.Named;
 import org.omnifaces.cdi.ViewScoped;
+
+import java.time.LocalDate;
+import java.util.Random;
 
 @Named
 @ViewScoped
@@ -17,5 +21,40 @@ public class ManageWorkshopsController extends CrudController<Workshop> {
     @Override
     protected CrudService<Workshop> getCrudService() {
         return manageWorkshopsService;
+    }
+
+    public void createRandomObjects(Integer quantity)
+    {
+        if (quantity == null)
+            return;
+
+        Random random = new Random(System.currentTimeMillis());
+
+        for (int i = 0; i < quantity; i++) {
+            int num = random.nextInt(1000);
+            Workshop obj = new Workshop();
+//            obj.setMyBoolean(num % 2 == 0);
+//            obj.setMyDate(LocalDate.now().plusDays(num));
+//            obj.setMyNumber(num);
+//            obj.setMyString("Random Object #" + num);
+            obj.setAcronym("FLA");
+            obj.setName("Testando #" + num);
+            obj.setYear(2025);
+            obj.setReviewDeadline(LocalDate.now().plusDays(num));
+            obj.setSubmissionDeadline(LocalDate.now().plusDays(num+5));
+
+            manageWorkshopsService.create(obj);
+
+            // If using regular listing, add the new object to the list.
+            if (entities != null)
+                entities.add(obj);
+        }
+
+        if (entities == null)
+            reloadLazyCount();
+
+        addGlobalI18nMessage("msgsCore", FacesMessage.SEVERITY_INFO,
+                "manageWorkshops.text.createRandomObjects.info.summary",
+                "manageWorkshops.text.createRandomObjects.info.detail");
     }
 }
